5b5b2f6d5d42c349656f811ed862059606dda062
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2022-03-08 08:46:38 +1000

    Added initial code for testing angular components

---
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -0,0 +1,31 @@
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { HeroComponent } from "./hero.component";
+
+describe("HeroComponent (shallow tests)", () => {
+  let fixture: ComponentFixture<HeroComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeroComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    });
+
+    fixture = TestBed.createComponent(HeroComponent);
+    // fixture.componentInstance.
+  });
+
+  it("should have the correct hero", () => {
+    fixture.componentInstance.hero = { id: 1, name: "superDude", strength: 3 };
+
+    expect(fixture.componentInstance.hero.name).toEqual("superDude");
+  });
+
+  it("should render the hero name in an anchor tag", () => {
+    fixture.componentInstance.hero = { id: 1, name: "superDude", strength: 3 };
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.css('a')).nativeElement.textContent).toContain('superDude')
+  });
+});
