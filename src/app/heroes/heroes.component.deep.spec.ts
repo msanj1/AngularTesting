28186d177b62df192d3c0a16b38af2c149bf1ae3
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2022-03-11 08:38:31 +1000

    added tests for triggering child event

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -23,23 +23,37 @@ describe("HeroesComponent (deep tests)", () => {
     TestBed.configureTestingModule({
       declarations: [HeroesComponent, HeroComponent],
       providers: [{ provide: HeroService, useValue: mockHeroService }],
-      schemas: [NO_ERRORS_SCHEMA]
+      schemas: [NO_ERRORS_SCHEMA],
     });
 
     fixture = TestBed.createComponent(HeroesComponent);
-
-
-   
   });
 
   it("should render each as a HeroComponent", () => {
     mockHeroService.getHeroes.and.returnValue(of(HEROES));
-    
+
     fixture.detectChanges();
-    const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+    const heroComponents = fixture.debugElement.queryAll(
+      By.directive(HeroComponent)
+    );
 
     expect(heroComponents.length).toBe(3);
-    expect(heroComponents[0].componentInstance.hero.name).toEqual('SpiderDude')
+    expect(heroComponents[0].componentInstance.hero.name).toEqual("SpiderDude");
     //can loop here
+  });
+
+  it(`should call hero service.delete when the Hero Component's delete button is clicked`, () => {
+    spyOn(fixture.componentInstance, "delete");
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+    fixture.detectChanges();
+
+    const heroComponents = fixture.debugElement.queryAll(
+      By.directive(HeroComponent)
+    );
+    // (<HeroComponent>heroComponents[0].componentInstance).delete.emit(undefined);
+    heroComponents[0].triggerEventHandler('delete', null);
+
+    expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[0]);
   });
 });
