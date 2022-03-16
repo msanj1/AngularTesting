c2c81b95d69e0ab9c56007c9f66e01b7a1755c14
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2022-03-17 08:37:00 +1000

    created a test coverage report

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,4 +1,11 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import {
+  ComponentFixture,
+  fakeAsync,
+  flush,
+  TestBed,
+  tick,
+  waitForAsync,
+} from "@angular/core/testing";
 import { ActivatedRoute } from "@angular/router";
 import { HeroService } from "../hero.service";
 import { HeroDetailComponent } from "./hero-detail.component";
@@ -44,4 +51,30 @@ describe("HeroDetailComponent", () => {
       "SUPERDUDE"
     );
   });
+
+  // it("should call updateHero when save is called", fakeAsync(() => {
+  //   mockHeroService.updateHero.and.returnValue(of({}));
+  //   fixture.detectChanges();
+
+  //   fixture.componentInstance.save();
+  //   flush();
+  //   // tick(250);
+
+  //   expect(mockHeroService.updateHero).toHaveBeenCalled();
+  // }));
+
+  it(
+    "should call updateHero when save is called",
+    waitForAsync(() => {
+      mockHeroService.updateHero.and.returnValue(of({}));
+      fixture.detectChanges();
+
+      fixture.componentInstance.save();
+      // flush();
+      // tick(250);
+      fixture.whenStable().then(() => {
+        expect(mockHeroService.updateHero).toHaveBeenCalled();
+      });
+    })
+  );
 });
